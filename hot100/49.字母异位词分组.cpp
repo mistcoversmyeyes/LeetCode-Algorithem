0b8220c9ff4901fc235e155d90f83c5a68b56900
yuming <mingjiangyu1@qq.com>
2026-02-18 01:25:09 +0800

    feat(26-2-18): 添加 leetcode 49 代码和题解

--- hot100/49.字母异位词分组.cpp
+++ hot100/49.字母异位词分组.cpp
@@ -5,6 +5,9 @@
  */
 
 // @lc code=start
+#include <algorithm>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 #include <string>
 
@@ -12,7 +15,30 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> result;
         
+        unordered_map<string, vector<int>> chrs2pos;
+        int index = 0;
+        for (auto str : strs) {
+            sort(str.begin(), str.end(), [](char lhs, char rhs){
+                return lhs < rhs;
+            });
+            if (chrs2pos.find(str) == chrs2pos.end()){
+                chrs2pos.insert({str, vector<int>()});
+            }
+            chrs2pos.find(str)->second.push_back(index);
+            index++;
+        }
+
+        for (const auto& item : chrs2pos) {
+            vector<string> tmp;
+            for (int idx : item.second){
+                tmp.push_back(strs.at(idx));
+            }            
+            result.push_back(tmp);
+        }
+
+        return result;
     }
 };
 // @lc code=end
