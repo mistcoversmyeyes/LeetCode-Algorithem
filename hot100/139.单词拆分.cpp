4c384b546aaa059d438083ecb03e53f104e0ef10
yuming <mingjiangyu1@qq.com>
2026-03-10 19:35:34 +0800

    feat: 添加 139. 单词拆分 的代码实现和题解

--- hot100/139.单词拆分.cpp
+++ hot100/139.单词拆分.cpp
@@ -5,9 +5,43 @@
  */
 
 // @lc code=start
+#include "string"
+#include "vector"
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
 class Solution {
 public:
+    bool wordBreak_memo(string& s, int k, unordered_set<string>& words, unordered_map<int, bool>& memo){
+        if (memo.count(k)) {
+            return memo[k];
+        }
+        if (k == s.length()) {
+            memo[k] = true;
+            return true;
+        }
+        
+        bool ret;
+        
+        for (int i = k; i < s.length(); i++) {
+            auto prefix = s.substr(k, i + 1 - k);
+            ret = words.count(prefix) &&  wordBreak_memo(s,i + 1 , words, memo);
+            if (ret) {
+                break;
+            }
+        }
+        memo.insert({k, ret});
+        return ret;
+    }
+    
     bool wordBreak(string s, vector<string>& wordDict) {
+        auto words = unordered_set<string>(wordDict.begin(), wordDict.end());
+        auto memo = unordered_map<int, bool>();
+
+        return wordBreak_memo(s,0, words, memo);
         
     }
 };
